84547f0d7852aaf58372f0c167b6899f728b69f8
Gisele Ribeiro <gisele@gisele.gisele>
2020-04-09 18:35:04 -0300

    update algotithm

--- mis.py
+++ mis.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 Maxset(G);
 Begin /* to return the size of the largest Independent Set of G*/ 
@@ -9,5 +11,39 @@ Begin /* to return the size of the largest Independent Set of G*/
 	n2 = Maxset(G – {v*} – Nb(v*)); 
 	Maxset = maximum_of(n1, n2); 
 end; 
-
 """
+
+#Algorithm
+
+#function to find the degree of a given graph
+def degree(graph):
+	l = 0
+	for i in graph.keys():
+		if(len(graph[i]) > l):
+			l = len(graph[i])
+	return l
+#function to find the vertex with highest degree of a given graph
+def findVertex(graph):
+	v = graph.keys()[0]
+	l = degree(graph)
+	for i in graph.keys():
+		if(len(graph[i]) == l):
+			v = i
+	return v	
+
+#function to find the maximum independent set of a given graph
+#def maxIndSet(graph):
+	
+
+graph = { "a" : ["c"],
+          "b" : ["c", "e"],
+          "c" : ["a", "b", "d", "e"],
+          "d" : ["c"],
+          "e" : ["c", "b"],
+          "f" : []
+        }
+	
+
+print(findVertex(graph))
+
+
