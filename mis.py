feb2b26c44ae3d4fae924b446a175675f33a371c
basilioarth <basilio.arth@gmail.com>
2020-11-23 22:30:29 -0300

    Update recursion function and ADD comments

--- mis.py
+++ mis.py
@@ -13,9 +13,10 @@ Begin /* to return the size of the largest Independent Set of G*/
 end; 
 """
 
-#Algorithm
+# Algorithm
 
-#function to find the degree of a given graph
+# function to find the degree of a given graph 
+# (will be the number of edges in the node with the highest number of neighboring)
 def degree(graph):
 	l = 0
 	for i in graph.keys():
@@ -23,7 +24,8 @@ def degree(graph):
 			l = len(graph[i])
 	return l
 
-#function to find the vertex with highest degree of a given graph
+# function to find the vertex with highest degree of a given graph 
+# (returns the last find vertex that has a degree equals to the graph´s degree)
 def findVertex(graph):
 	l = degree(graph)
 	for i in graph.keys():
@@ -31,7 +33,7 @@ def findVertex(graph):
 			v = i
 	return v	
 
-#deleta todas as referencias ao vertice v no grafo 
+# function that deletes all references to vertex v in the graph 
 def delete(graph, v):
 	graph.pop(v)
 	for i in graph.keys():
@@ -40,23 +42,26 @@ def delete(graph, v):
 				graph[i].remove(j)
 	return graph
 
+# function that deletes all neighbors (and their references) of a node
 def deleteVizinhos(graph, v):
 	for i in range (0, len(graph[v])):
 		graph = delete(graph, graph[v][0])
+		# a cada deletar os valores da chave em questão (representa o vértice que terá seus vizinhos excluídos)
+		# vão automaticamente se deslocando para o início da fila, pois o espaço da primeiro posição foi liberado 
 	return graph
 
 def maxIndSet1(graph):
 	if(degree(graph) == 0):
 		return graph
 	v = findVertex(graph)
-	n1 = maxIndSet1(delete(graph, v))
+	maxIndSet1(delete(graph, v))
 	return graph
 
 def maxIndSet2(graph):
 	if(degree(graph) == 0):
 		return graph
 	v = findVertex(graph)
-	n1 = maxIndSet1(deleteVizinhos(graph, v))
+	maxIndSet2(deleteVizinhos(graph, v))
 	return graph
 
 #function to find the maximum independent set of a given graph
@@ -65,6 +70,13 @@ def maxIndSet(graph):
 	graph2 = dict(graph)
 	n1 = maxIndSet1(graph1)
 	n2 = maxIndSet2(graph2)
+	print("Conjunto Independente I: ")
+	print(n1)
+	print()
+	print("Conjunto Independente II: ")
+	print(n2)
+	print()
+	print("Conjunto Independente Máximo: ")
 	return graph1 if(len(n1) > len(n2)) else graph2
 
 graph = { "a" : ["c"],
@@ -77,5 +89,3 @@ graph = { "a" : ["c"],
 	
 
 print(maxIndSet(graph))
-
-
